181cee7ac203cc731b0420d323f07a959e794aae
Alan Verissimo <alan_verissimo@hotmail.com>
2022-06-18 22:41:52 -0300

    Done!

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,14 @@
-export function helloWorld(request, response) {
+import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
+
+export function helloWorld(request: Request, response: Response) {
+    const user = createUser({
+        email: 'alan@hotmail.com',
+        password: '123456',
+        techs: ['Node.js', 'ReactJS', 'React Native', {title: 'JavaScript', experience: 100 },
+        ]
+    });
+
     return response.json({ message: 'Hello world' });
+
 };
